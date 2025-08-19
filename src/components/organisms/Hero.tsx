ad10339b81bc4780b6d340f4e10e161eaea7c5a0
Mattia-FR <mattia.bouvier@protonmail.com>
2025-08-19 17:25:09 +0200

    Outlet done. First route done. Working on the HomePage.

---
+++ src/components/organisms/Hero.tsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <section>
+      <h1>Titre</h1>
+      <p>Ici on est dans Hero.</p>
+      {/* Au final, il va falloir une grande image, une catch phrase et une invitation au scroll ici. */}
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Hero from "../organisms/Hero";
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <Hero />
+    </div>
+  );
+}
+
+export default HomePage;
