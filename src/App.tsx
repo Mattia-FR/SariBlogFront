c3561b2de168e3b8bef4f9861ee6c36c0b455043
Mattia-FR <mattia.bouvier@protonmail.com>
2025-08-19 17:36:13 +0200

    Outlet done. First route done. Working on the HomePage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <main>
+      <Outlet />
+    </main>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.tsx";
+
+import HomePage from "./components/pages/HomePage.tsx";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 const rootElement = document.getElementById("root");
 
 if (rootElement == null) {
-	throw new Error(`Your HTML Document must contain a <div id="root"></div>`);
+  throw new Error(`Your HTML Document must contain a <div id="root"></div>`);
+} else {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
 }
-
-createRoot(rootElement).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
