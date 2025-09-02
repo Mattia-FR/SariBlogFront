5ef8ee437cb0a2fc75f36f98fcfac676594d2b8c
Mattia-FR <mattia.bouvier@protonmail.com>
2025-09-03 00:29:12 +0200

    CSS, again.

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -1,15 +1,21 @@
+import heroImage from "../../assets/hero.jpg";
 import ScrollButton from "../atoms/ScrollButton";
 import "./Hero.css";
 
 function Hero() {
   return (
     <section className="hero-full">
-      <h2>
-        "Un peu gluant mais appétissant !"
-        <br />
-        Nelson Mandela
-      </h2>
-      <ScrollButton />
+      <section className="hero-left">
+        <h2>
+          "Un peu gluant mais appétissant !"
+          <br />
+          Nelson Mandela
+        </h2>
+        <ScrollButton />
+      </section>
+      <section className="hero-right">
+        <img src={heroImage} alt="Dessin en noir et blanc d'une poupée" />
+      </section>
     </section>
   );
 }
