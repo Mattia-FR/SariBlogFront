d11dad29a6d656f3d3e9abcff2d492b0d5699d3f
Mattia-FR <mattia.bouvier@protonmail.com>
2026-04-18 16:24:59 +0200

    Fix.

--- src/components/pages/ErrorPage/ErrorPage.tsx
+++ src/components/pages/ErrorPage/ErrorPage.tsx
@@ -1,18 +1,20 @@
-import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
 import "./ErrorPage.css";
 
 export function ErrorPage() {
   const error = useRouteError();
 
   let title = "Oups !";
-  let message = "Une erreur inattendue est survenue. Rechargez la page ou revenez à la page d'accueil.";
+  let message =
+    "Une erreur inattendue est survenue. Rechargez la page ou revenez à la page d'accueil.";
 
   if (isRouteErrorResponse(error)) {
     if (error.status === 404) {
       title = "Page introuvable";
       message = "Cette page n'existe pas ou n'est plus disponible.";
     } else {
-      message = "Le serveur a rencontré un problème. Réessayez dans quelques instants.";
+      message =
+        "Le serveur a rencontré un problème. Réessayez dans quelques instants.";
     }
   }
 
